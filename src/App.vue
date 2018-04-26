29528e43e322d369ace8cdd9c580e111c7ae3a34
feigebaobei <18515195415@163.com>
2018-04-27 07:47:54 +0800

    页面链接中添加查询字符串

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <img src="./assets/logo.png">
     <router-view/>
     <div>
-      <a href="./first.html">first</a>
+      <a href="./first.html?name=lee&age=28">first</a>
       <a href="./second.html">second</a>
     </div>
   </div>
